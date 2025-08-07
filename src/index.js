5205d032ea194ab34cbde6b973f450cb740c04e0
Mohd Zama <zama9170@gmail.com>
2025-08-08 01:49:58 +0530

    Init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv");
 
-dotenv.config();
+dotenv.config({ path: "../.env" });
 
 const express = require("express");
 const cors = require("cors");
--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,19 +1,21 @@
-const jwt = require("jsonwebtoken");
 const { z } = require("zod");
+const jwt = require("jsonwebtoken");
 
-function auth(req, res, next) {
-	let token = req.headers["authorization"].split(" ")[1];
-	try {
-		token = z.string().parse(token);
-		const decodePayload = jwt.verify(token, JWT_USER);
-		req.userId = decodePayload.userId;
-		next();
-	} catch (error) {
-		res.status(403).json({ message: "Forbidden: invalid token!", error });
+const AppError = require("../utils/AppError");
+
+function auth(req, _, next) {
+	let token = req.headers.token;
+
+	if (!token) {
+		throw new AppError(403, "Invalid Token", "Forbidden: invalid token!");
 	}
+
+	token = z.string().parse(token);
+
+	const decodePayload = jwt.verify(token, process.env.JWT_USER);
+
+	req.userId = decodePayload.userId;
+	next();
 }
 
-module.exports = {
-	auth,
-	JWT_USER,
-};
+module.exports = { auth };
